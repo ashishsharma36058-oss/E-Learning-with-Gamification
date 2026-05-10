29cdb9d08df8f8d4ab1a7cf5c1196a42ab65baa0
ashishsharma36058-oss <ashishsharma36058@gmail.com>
2026-05-10 12:41:14 +0530

    Update middleware.py

--- app/core/middleware.py
+++ app/core/middleware.py
@@ -5,7 +5,7 @@ import time
 def setup_middleware(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["GET", "POST", "PUT", "DELETE"],
         allow_headers=["Authorization", "Content-Type"],
