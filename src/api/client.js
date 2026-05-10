82ed8192de893a380a6bb87a25fdbf860bf1cdf6
ashishsharma36058-oss <ashishsharma36058@gmail.com>
2026-05-10 12:06:42 +0530

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api', timeout: 15000 })
+const api = axios.create({
+  baseURL: 'https://e-learning-with-gamification-2.onrender.com/api',
+  timeout: 15000
+})
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('g_access')
@@ -17,7 +20,7 @@ api.interceptors.response.use(
       try {
         const refresh = localStorage.getItem('g_refresh')
         if (!refresh) throw new Error('no refresh')
-        const { data } = await axios.post('/api/auth/refresh', { refresh_token: refresh })
+        const { data } = await axios.post('https://e-learning-with-gamification-2.onrender.com/api/auth/refresh', { refresh_token: refresh })
         localStorage.setItem('g_access', data.access_token)
         localStorage.setItem('g_refresh', data.refresh_token)
         original.headers.Authorization = `Bearer ${data.access_token}`
