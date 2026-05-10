845b9a1e3466f05a20730ce84dc51c93714f2d59
ashishsharma36058-oss <ashishsharma36058@gmail.com>
2026-05-10 12:34:37 +0530

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://e-learning-with-gamification-2.onrender.com/api',
+  baseURL: 'https://e-learning-with-gamification-2.onrender.com',
   timeout: 15000
 })
 
