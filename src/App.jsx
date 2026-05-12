e4e90012cfe7aeaa11b8089fba3d55b474525b51
ashishsharma36058-oss <ashishsharma36058@gmail.com>
2026-05-12 16:00:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Auth from "./pages/Auth";
+import * as AuthModule from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import GamePlay from "./pages/GamePlay";
 import Leaderboard from "./pages/Leaderboard";
 import Play from "./pages/Play";
 
+const Auth = AuthModule.default || AuthModule.Auth || AuthModule.Login;
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Login */}
         <Route path="/" element={<Auth />} />
         <Route path="/login" element={<Auth />} />
         <Route path="/register" element={<Auth />} />
 
-        {/* Main Pages */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/play" element={<Play />} />
         <Route path="/game" element={<GamePlay />} />
         <Route path="/rankings" element={<Leaderboard />} />
-
       </Routes>
     </BrowserRouter>
   );
